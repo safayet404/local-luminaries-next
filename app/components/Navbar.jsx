08c1b4a7083ddb63e33d0d4daa952d556a649d69
Safayet <hossainsafayet187@gmail.com>
2025-04-22 13:50:29 +0600

    navbar created

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,60 @@
+"use client"
+
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+
+
+const Navbar = () => {
+    const [isMenuOpen,setIsMenuOpen] = useState(false)
+
+    const toggleMenu =() => setIsMenuOpen(!isMenuOpen)
+  return (
+    <div
+    class=" fixed top-0  left-0 right-0 bg-sky-800 z-50"
+>
+<div
+        class="navbar container mx-auto flex justify-between items-center px-4 py-7"
+    >
+
+        <div>Local Luminaries</div>
+
+        <button onClick={toggleMenu} className='lg:hidden btn'>
+        {isMenuOpen ? '✖' : '☰'}
+        </button>
+
+        <div className='hidden lg:flex'>
+            <ul className='flex space-x-10 font-medium uppercase'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Tours</li>
+                <li>Team</li>
+                <li>Contact</li>
+                <li>Profile</li>
+            </ul>
+        </div>
+
+        <div className='hidden lg:flex'>
+            <Link href="/profile">Profile</Link>
+        </div>
+
+      </div>
+
+      {isMenuOpen && (
+                <div className="lg:hidden fixed top-16 right-4 bg-white w-64 p-4 rounded-lg shadow-lg z-40 animate-slide-in">
+                    <ul className="space-y-4 text-left text-[#1F2224]">
+                        <li><Link href="/">Home</Link></li>
+
+                   
+                        <li><Link href="/case">Case Studies</Link></li>
+
+                      
+                        <li><Link href="/resources">Resources</Link></li>
+                    </ul>
+                </div>
+            )}
+    </div>
+  )
+}
+
+export default Navbar
